d6df5d7713fdd243abbaeda6d3ddbf6fd82d3b02
AlisonDias <36650633+AlisonDias@users.noreply.github.com>
2019-09-05 21:02:34 -0300

    Create ProjetoGame.sql

---
+++ ProjetoGame.sql
@@ -0,0 +1,85 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.6-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Copiando estrutura do banco de dados para projetogame
+CREATE DATABASE IF NOT EXISTS `projetogame` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `projetogame`;
+
+-- Copiando estrutura para tabela projetogame.game
+CREATE TABLE IF NOT EXISTS `game` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nomeGame` varchar(255) DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `imgCapa` varchar(255) DEFAULT NULL,
+  `disponivel` int(1) DEFAULT NULL,
+  `usuario_id` int(10) DEFAULT NULL,
+  `genero_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela projetogame.genero
+CREATE TABLE IF NOT EXISTS `genero` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nomeGenero` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela projetogame.situacao
+CREATE TABLE IF NOT EXISTS `situacao` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `situacao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela projetogame.solicitacao
+CREATE TABLE IF NOT EXISTS `solicitacao` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `data` date DEFAULT NULL,
+  `usuario_id` int(10) DEFAULT NULL,
+  `genero_id` int(10) DEFAULT NULL,
+  `situacao_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela projetogame.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `rua` varchar(255) DEFAULT NULL,
+  `bairro` varchar(255) DEFAULT NULL,
+  `cidade` varchar(255) DEFAULT NULL,
+  `casa` int(12) DEFAULT NULL,
+  `estado` varchar(2) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `cep` int(8) DEFAULT NULL,
+  `telefone` int(12) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL,
+  `imgPerfil` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Exportação de dados foi desmarcado.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
