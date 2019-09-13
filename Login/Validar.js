2cf0f2d21ed1e3d91f4d87651eea3305536486a3
Giovane Oliveira <35038647+Giovane-Oliveira@users.noreply.github.com>
2019-09-13 05:57:15 -0300

    Update

---
+++ Login/Validar.js
@@ -0,0 +1,13 @@
+
+function validar(n) {
+
+if ( n == false ){
+
+  var p1 = window.document.getElementById('aviso')
+  p1.innerHTML = 'Usuário ou senha inválido!'
+  p1.style.background = "yellow"
+  p1.style.color = 'black'
+
+}
+
+ }
