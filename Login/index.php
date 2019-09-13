0cccca8bfa8eaa28248da1ca96cf6f0dbf51e507
Giovane Oliveira <35038647+Giovane-Oliveira@users.noreply.github.com>
2019-09-13 06:37:49 -0300

    Update

--- Login/index.php
+++ Login/index.php
@@ -9,7 +9,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="Login.css">
 <script src="Validar.js" charset="utf-8"></script>
-</script>
   </head>
   <?php session_start(); ?>
   <body onload="validar('<?php echo $_SESSION['logado'];?>')">
