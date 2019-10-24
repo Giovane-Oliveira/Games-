f1a8abdecfd028a3b5e229db2fe54af036c04551
AlisonDias <36650633+AlisonDias@users.noreply.github.com>
2019-10-24 20:32:32 -0300

    Update jogos.php

--- jogos/jogos.php
+++ jogos/jogos.php
@@ -17,22 +17,14 @@
 <!-- BARRA DE NAVEGAÇÃO -->
  <?php
 
-  include 'barraNavegacao.php';
+  include '../principal/barraNavegacao.php';
 
 ?> 
 
 <!-- CORPO PRINCIPAL -->
 
-<div class="container">
-  <div class="row">
-    <div class="col">
 
-                  <?php  
-                  
-                  include '../usuarios/buscaDadosUsuario.php';
 
-                  ?>
-    </div>
     <div class="col-6">
       <h1>Jogos</h1>
       <div class="tableUsuarios"> 
@@ -58,34 +50,6 @@
           </table>
       </div>
     </div>
-    <div class="col">
-      <h1>Emprestimos</h1>
-      <div class="tableUsuarios"> 
-        <table class="table">
-          <thead class="thead-dark">
-          <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Jogo</th>
-              <th scope="col">Dono</th>
-              <th scope="col">Status</th>
-          </tr>
-          </thead>
-          <tbody>
-
-                      <?php
-
-                       include '../usuarios/buscaEmprestimosJogos.php'
-
-                      ?>
-
-            </tbody>
-          </table>
-      </div>
-    </div>
-
-
-
-
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
