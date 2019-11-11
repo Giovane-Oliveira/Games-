4d6eb080670787b24d4844d7c5710df851d44c4c
AlisonDias <36650633+AlisonDias@users.noreply.github.com>
2019-11-11 17:16:07 -0300

    Update buscaEmprestimosJogos.php

--- usuarios/buscaEmprestimosJogos.php
+++ usuarios/buscaEmprestimosJogos.php
@@ -14,26 +14,33 @@ $consulta_emprestimo = "SELECT * FROM solicitacao WHERE usuario_id = $_SESSION[i
 $resultado_emprestimo = mysqli_query($conecta, $consulta_emprestimo);
 
 }
-?>
 
-	<?php while($resultado = mysqli_fetch_array($resultado_emprestimo)) {
+
+ while($resultado = mysqli_fetch_array($resultado_emprestimo)) {
 
 		$consulta_game = "SELECT * FROM game WHERE id = $resultado[game_id];";
 		$resultado_game = mysqli_query($conecta, $consulta_game);
 		$resultado2 =  mysqli_fetch_array($resultado_game);
 
-		$consulta_usuario = "SELECT * FROM usuario WHERE id = $resultado[usuario_id];";
+		$consulta_usuario = "SELECT * FROM usuario WHERE id = $resultado2[usuario_id];";
 		$resultado_usuario = mysqli_query($conecta, $consulta_usuario);
 		$resultado3 =  mysqli_fetch_array($resultado_usuario);
 
-	?>
+		$consulta_genero = "SELECT * FROM genero WHERE id = $resultado2[genero_id];";
+		$resultado_genero = mysqli_query($conecta, $consulta_genero);
+		$resultado4 =  mysqli_fetch_array($resultado_genero);
+
+?>
 	<tr>
-      <td><?php echo $resultado['id']; ?></td>
+      <td><?php echo $resultado['data']; ?></td>
+      <td><?php echo $resultado2['nomeGame']; ?></td>
       <td><img src="../<?php echo $resultado2['imgCapa'] ?>" alt="Foto Game" class="rounded-circle" width="30" height="30"></td>
+      <td><?php echo $resultado4['nomeGenero']; ?></td>
+      <td><?php echo $resultado3['nome']; ?></td>
       <td><img src="<?php echo $resultado3['imgPerfil'] ?>" alt="Foto Game" class="rounded-circle" width="30" height="30"></td>
-      <td><?php echo $resultado['situacao_id']; ?></td>
+
+      <td><?php if($resultado['situacao_id'] == 1){echo 'Livre';}else if ($resultado['situacao_id'] == 2) { echo 'Indisponivel';} else {echo 'Solicitado';} ?></td>
+      
       
 	</tr>
   <?php } ?>
-      	
-
