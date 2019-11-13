ace67abd7ad20c0f40f9ecbdbf4715284b3fc6c3
AlisonDias <36650633+AlisonDias@users.noreply.github.com>
2019-11-12 21:31:24 -0300

    Solicitação de emprestimo

--- jogos/emprestimo.php
+++ jogos/emprestimo.php
@@ -45,11 +45,62 @@ if($_SESSION['logado'] == true){
       </div>
       <p><?php echo $resultado['descricao'] ?></p>
 
-      <a class="btn btn-dark btn-lg btn-block" href="solicitaEmprestimo.php?idJogo=<?php echo($resultado_id)?>" role="button">Solicitar Emprestimo</a>
+      <a class="btn btn-dark btn-lg btn-block" href="solicitaEmprestimo.php?idJogo=<?php echo($resultado_id)?>" role="button" data-toggle="modal" data-target="#exampleModal">Solicitar Emprestimo</a>
       <p></p>
       <p></p>
     </div>
     <div class="col">
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Você realmente quer solicitar o emprestimo do Jogo?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <a href="solicitaEmprestimo.php?idJogo=<?php echo($resultado_id)?>" type="button" class="btn btn-primary">Solicitar emprestimo</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+    
+    <?php
+    echo $_SESSION['solicitado'];
+      if ($_SESSION['solicitado']){
+      ?>
+        <div class="modal" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <?php  }
+    ?>
       
 
 
--- jogos/solicitaEmprestimo.php
+++ jogos/solicitaEmprestimo.php
@@ -3,13 +3,19 @@
 $id = $_GET['idJogo'];
 $data = date('Y-m-d');
 $usuario = $_SESSION['id'];
+$_SESSION['solicitado'] = true;
 
 $consulta_game = "SELECT * FROM game WHERE genero_id = '$id';";
 $resultado_game = mysqli_query($conecta, $consulta_game);
 $resultado = mysqli_fetch_array($resultado_game);
 
-$sql = "INSERT INTO solicitacao (data, usuario_id, game_id) VALUES ('$data', '$usuario', '$id', '3');";
-mysqli_query($conecta, $sql)
+$sql = "INSERT INTO solicitacao (data, usuario_id, game_id, situacao_id) VALUES ('$data', '$usuario', '$id', 3);";
+mysqli_query($conecta, $sql);
+
+$sqlAtualizacaoGame = "UPDATE game SET disponivel = 1 WHERE id = $id;";
+mysqli_query($conecta, $sqlAtualizacaoGame);
+
+header('Location: emprestimo.php?id='.$id);
 
 
 ?>
