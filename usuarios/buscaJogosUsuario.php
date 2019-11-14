fc3f86fdf507515185e5bd2109c224038d51368d
Alison Dias <alisoondias@gmail.com>
2019-11-14 19:23:47 -0300

    Alterações

--- usuarios/buscaJogosUsuario.php
+++ usuarios/buscaJogosUsuario.php
@@ -23,6 +23,7 @@ $resultado_game = mysqli_query($conecta, $consulta_game);
       <td><?php echo $resultado['nomeGame']; ?></td>
       <td><?php echo $resultado['descricao']; ?></td>
       <td><?php if($resultado['disponivel'] == 0){echo 'Disponivel';}else{ echo 'Ocupado';} ?></td>
+      <td><a href="../Principal/visualizarJogo.php?id=<?php echo($resultado['id'])?>"><img src="../imagens/icon/iconPesquisa.png" width="15" height="15"></a></td>
 	</tr>
   <?php } ?>
       	
