4993a52c4cf40363ffdb66bdd883caf7376d205d
MDener <54964005+MDener@users.noreply.github.com>
2019-11-15 19:49:08 -0300

    Update buscaUsuarios.php

--- Principal/buscaUsuarios.php
+++ Principal/buscaUsuarios.php
@@ -4,11 +4,10 @@ $result = mysqli_query($conecta, $consulta);
 ?>
 
 <?php while($resultado = mysqli_fetch_array($result)) { 
-	
 	$usuario_id =  $resultado['id']?>
 	<tr>
-      	<td scope="row"><img src="<?php echo $resultado['imgPerfil'] ?>" alt="Foto Usuario" class="rounded-circle" width="15" height="15"></td>
-      	<td><?php echo $resultado['nome']; ?></td>
+		<td scope="row"><img src="<?php echo $resultado['imgPerfil'] ?>" alt="Foto Usuario" class="rounded-circle" width="15" height="15"></td>
+		<td><?php echo $resultado['nome']; ?></td>
 		<td><a href="perfilUsuario.php?idUsuario=<?php echo($usuario_id)?>"><img src="../imagens/icon/iconPesquisa.png" width="20" height="20"></a></td>
 	</tr>
 <?php } ?>
