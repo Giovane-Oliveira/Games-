c0102f7d93c8e61646bfdaeadc719438761f670c
AlisonDias <36650633+AlisonDias@users.noreply.github.com>
2019-11-18 20:19:30 -0300

    Atualização

--- Principal/buscaUsuarios.php
+++ Principal/buscaUsuarios.php
@@ -5,9 +5,15 @@ $result = mysqli_query($conecta, $consulta);
 
 <?php while($resultado = mysqli_fetch_array($result)) { 
 	$usuario_id =  $resultado['id']?>
-	<tr>
-		<td scope="row"><img src="<?php echo $resultado['imgPerfil'] ?>" alt="Foto Usuario" class="rounded-circle" width="15" height="15"></td>
-		<td><?php echo $resultado['nome']; ?></td>
-		<td><a href="perfilUsuario.php?idUsuario=<?php echo($usuario_id)?>"><img src="../imagens/icon/iconPesquisa.png" width="20" height="20"></a></td>
+	<tr>	
+		<th>
+			<button type="button" class="btn btn-link ">
+				<a style="white-space: nowrap" href="perfilUsuario.php?idUsuario=<?php echo($usuario_id)?>">	
+					<img src="../imagens/icon/iconPesquisa.png" width="20" height="20">&nbsp&nbsp
+					<img src="<?php echo $resultado['imgPerfil'] ?>" alt=" " class="rounded-circle" width="30">
+					<text class="text-dark">&nbsp&nbsp<?php echo $resultado['nome']; ?></text>					
+				</a>
+			</button>
+		</th>
 	</tr>
 <?php } ?>
