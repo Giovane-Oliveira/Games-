c8ff801b5156d619e9ad9278eecbabb3c61fcd2f
AlisonDias <36650633+AlisonDias@users.noreply.github.com>
2019-11-20 15:05:32 -0300

    Update buscaEmprestimosJogos.php

--- jogos/buscaEmprestimosJogos.php
+++ jogos/buscaEmprestimosJogos.php
@@ -1,28 +1,19 @@
 <?php
 
-if(isset($_GET['idUsuario'])){
 
-$idUsuario = $_GET['idUsuario'];	
-
-$consulta_emprestimo = "SELECT * FROM solicitacao WHERE usuario_id = $idUsuario;";
-$resultado_emprestimo = mysqli_query($conecta, $consulta_emprestimo);
-
-}else{
+$consulta_game = "SELECT * FROM game WHERE usuario_id = $_SESSION[id] and disponivel = '1';";
+$resultado_game = mysqli_query($conecta, $consulta_game);
 
 
-$consulta_emprestimo = "SELECT * FROM solicitacao WHERE usuario_id = $_SESSION[id];";
-$resultado_emprestimo = mysqli_query($conecta, $consulta_emprestimo);
-
-}
 
 
- while($resultado = mysqli_fetch_array($resultado_emprestimo)) {
+ while($resultado = mysqli_fetch_array($resultado_game)) {
 
-		$consulta_game = "SELECT * FROM game WHERE id = $resultado[game_id];";
-		$resultado_game = mysqli_query($conecta, $consulta_game);
-		$resultado2 =  mysqli_fetch_array($resultado_game);
+		$consulta_solicitacao = "SELECT * FROM solicitacao WHERE game_id = $resultado[id];";
+		$resultado_solicitacao = mysqli_query($conecta, $consulta_solicitacao);
+		$resultado2 =  mysqli_fetch_array($resultado_solicitacao);
 
-		$consulta_usuario = "SELECT * FROM usuario WHERE id = $resultado[usuario_id];";
+		$consulta_usuario = "SELECT * FROM usuario WHERE id = $resultado2[usuario_id];";
 		$resultado_usuario = mysqli_query($conecta, $consulta_usuario);
 		$resultado3 =  mysqli_fetch_array($resultado_usuario);
 
@@ -31,12 +22,12 @@ $resultado_emprestimo = mysqli_query($conecta, $consulta_emprestimo);
 ?>
 	<tr>
       
-      <td><?php echo $resultado2['nomeGame']; ?></td>
+      <td><?php echo $resultado['nomeGame']; ?></td>
 
-      <td><img src="../<?php echo $resultado2['imgCapa'] ?>" alt="Foto Game" class="rounded-circle" width="30" height="30"></td>
-      <td><?php echo $resultado['data']; ?></td>
+      <td><img src="../<?php echo $resultado['imgCapa'] ?>" alt="Foto Game" class="rounded-circle" width="30" height="30"></td>
+      <td><?php echo $resultado2['data']; ?></td>
       <td><?php echo $resultado3['nome']; ?></td>
-      <td><img src="<?php echo $resultado3['imgPerfil'] ?>" alt="Foto Game" class="rounded-circle" width="30" height="30"></td>
+      <td><img src="<?php echo $resultado3['imgPerfil'] ?>" alt="Foto Perfil" class="rounded-circle" width="30" height="30"></td>
 
       
       
