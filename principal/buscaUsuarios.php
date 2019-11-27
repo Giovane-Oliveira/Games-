9186ffaf9872e7d4c64f3a56a3f1cb7b9f39b02f
MDener <54964005+MDener@users.noreply.github.com>
2019-11-26 21:49:30 -0300

    Update buscaUsuarios.php

--- principal/buscaUsuarios.php
+++ principal/buscaUsuarios.php
@@ -6,14 +6,12 @@ $result = mysqli_query($conecta, $consulta);
 <?php while($resultado = mysqli_fetch_array($result)) { 
 	$usuario_id =  $resultado['id']?>
 	<tr>	
-		<th>
-			<button type="button" class="btn btn-link ">
-				<a style="white-space: nowrap" href="perfilUsuario.php?idUsuario=<?php echo($usuario_id)?>">	
+		<th>			
+				<a style="white-space: nowrap" class="btn" href="perfilUsuario.php?idUsuario=<?php echo($usuario_id)?>">	
 					<img src="../imagens/icon/iconPesquisa.png" width="20" height="20">&nbsp&nbsp
 					<img src="<?php echo $resultado['imgPerfil'] ?>" alt=" " class="rounded-circle" width="30">
 					<text class="text-dark">&nbsp&nbsp<?php echo $resultado['nome']; ?></text>					
-				</a>
-			</button>
+				</a>			
 		</th>
 	</tr>
 <?php } ?>
